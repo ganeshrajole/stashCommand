bed70a9f0f80a053b7e1149b9f01c526ae401f73
Ganesh Rajole <ganeshrajole@gmail.com>
2022-11-02 12:57:29 +0530

    Another New File Created

---
+++ Demo1.java
@@ -0,0 +1,28 @@
+public class Demo1 {
+
+public static void main(String[] args) {
+
+int n=20;
+
+
+if(n%2==0) {
+
+syso("Entered Number Is Even")
+
+
+}
+
+else {
+
+syso("Entered Number Is Odd")
+
+}
+
+
+
+}
+
+
+
+
+}
